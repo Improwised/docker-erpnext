6d1ebb08c916a28a6876277a077894edff869ac7
Gonzalo González Domínguez <meigallo@meigallodixital.com>
2017-08-17 13:31:07 +0200

    Prefixed entrypoint with shell and dockerize with path

--- Docker/erpnext/docker-entrypoint.sh
+++ Docker/erpnext/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 bench use localhost
 
 # setup site_config.json
-dockerize -template /tmp/config/site_config.json.tmpl:/home/$FRAPPE_USER/frappe-bench/sites/common_site_config.json \
+/usr/local/bin/dockerize -template /tmp/config/site_config.json.tmpl:/home/$FRAPPE_USER/frappe-bench/sites/common_site_config.json \
           -template /tmp/config/site_config.json.tmpl:/home/$FRAPPE_USER/frappe-bench/sites/localhost/site_config.json \
           true
 
@@ -12,11 +12,10 @@ chmod 440 /home/$FRAPPE_USER/frappe-bench/sites/common_site_config.json \
 
 
 echo 'Waiting for DB to start up'
-dockerize -wait tcp://db:3306 -timeout 120s
+/usr/local/bin/dockerize -wait tcp://db:3306 -timeout 120s
 
 echo 'Erpnext install'
 cd /home/frappe/frappe-bench && \
 bench get-app erpnext $ERPNEXT_REPO --branch $ERPNEXT_BRANCH && \
-bench reinstall && \
 bench install-app erpnext && \
 supervisor -c /etc/supervisor.conf
